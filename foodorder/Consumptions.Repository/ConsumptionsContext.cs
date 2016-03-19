e5b6a37c7e0d7a3f694b1790d09b88488783a39e
vekkel <vekkel@twinfield.com>
2016-03-19 10:35:59 +0100

    Cleaned things up like usings and example files

--- foodorder/Consumptions.Repository/ConsumptionsContext.cs
+++ foodorder/Consumptions.Repository/ConsumptionsContext.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Data.Entity;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Consumptions.Repository
 {
@@ -14,6 +9,6 @@ namespace Consumptions.Repository
 		public ConsumptionsContext() : base((new SqlConnectionStringBuilder(
 				ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString)).ToString())
 		{ }
-		public DbSet<Consumption> Orders { get; set; }
+		public DbSet<Consumption> Consumptions { get; set; }
 	}
 }
